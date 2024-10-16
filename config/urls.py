623dcb89e9bffe3ecd683dd16349e07fd528a33e
Artur <kokivirivoki@mail.ru>
2024-10-16 13:17:00 +0500

    Create project

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('', include('electronic.urls', namespace='main')),
+                  path('users/', include('users.urls', namespace='users'))] + static(settings.MEDIA_URL,
+                                                                                     document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
